55cb541b5ad523860b72c3804d67b3032b9f3408
Zeq <Ezequiellopez.dev@gmail.com>
2023-11-26 14:50:29 -0300

    To-do list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
+import { MdDelete } from 'react-icons/md'
+import { ImCheckboxChecked, ImCheckboxUnchecked } from 'react-icons/im'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Todos {
+	id: number
+	body: string
+	complete: boolean
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App() {
+	const [input, setInput] = useState<string>('')
+	const [todos, setTodo] = useState<Todos[]>(
+		!localStorage.getItem('todos') ? [] : JSON.parse(localStorage.getItem('todos')!)
+	)
+
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos))
+	}, [todos])
+
+	const addTask = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter' && input.length !== 0) {
+			const newTask: Todos = {
+				id: Math.random() * 1000,
+				body: input.trim().charAt(0).toUpperCase() + input.trim().slice(1),
+				complete: false,
+			}
+
+			setTodo([...todos, newTask])
+			setInput('')
+		}
+	}
+
+	const rmTask = (id: number) => {
+		const rmTask = todos.filter(item => item.id !== id)
+		setTodo(rmTask)
+	}
+
+	const isComplete = (id: number) => {
+		setTodo(
+			todos.map(item => {
+				if (item.id === id) {
+					return {
+						...item,
+						complete: !item.complete,
+					}
+				}
+				return item
+			})
+		)
+	}
+
+	return (
+		<section className='container'>
+			<header>
+				<h1 className='title'>What's the plan for today?</h1>
+				<input
+					className='input'
+					type='text'
+					placeholder='Add a task'
+					onChange={e => setInput(e.target.value)}
+					value={input}
+					onKeyPress={addTask}
+				/>
+			</header>
+			<main className='todo-list'>
+				{todos.map(item => {
+					return (
+						<article className='todo-item' key={item.id}>
+							<p className='todo-txt'>{item.body}</p>
+							<div className='actions'>
+								{item.complete === false ? (
+									<ImCheckboxUnchecked
+										size={20}
+										className='btn-complete'
+										onClick={() => isComplete(item.id)}
+									/>
+								) : (
+									<ImCheckboxChecked
+										size={20}
+										className='btn-complete'
+										onClick={() => isComplete(item.id)}
+									/>
+								)}
+								<MdDelete
+									size={25}
+									className='btn-delete'
+									onClick={() => rmTask(item.id)}
+								/>
+							</div>
+						</article>
+					)
+				})}
+			</main>
+		</section>
+	)
 }
 
 export default App
